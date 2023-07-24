847f2609dd5e11f5fb339b5c21d92778b210936b
AndyL86 <alinehan89@outlook.com>
2023-07-24 11:49:52 +0000

    state

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  // function clickHandler() {}
-  const [title, setTitle] = useState(props.title);
-
-  function ClickHandler() {
-    setTitle('Clicked Bitch!');
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate  date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-      <button onClick={ClickHandler}>Change Title</button>
     </Card>
   );
 }
